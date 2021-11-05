102cdff8c01988ff9af6afd764ab25c7a7973143
Dinaerte Neto <dinaerteneto@gmail.com>
2021-11-05 15:06:25 -0300

    refactor: clean code

--- tests/usecases/authorization.spec.ts
+++ tests/usecases/authorization.spec.ts
@@ -13,24 +13,30 @@ jest.mock('jsonwebtoken', () => ({
     }
 }))
 
+const makeSut = () => {
+    const jwtAdapter = new JwtAdapter('secret')
+    const sut = new Authorization(jwtAdapter)
+    return {
+        jwtAdapter,
+        sut
+    }
+}
+
 describe('Authorization UseCase', () => {
     test('Should call JwtAdapter with valid token', async () => {
-        const jwtAdapter = new JwtAdapter('secret')
-        const sut = new Authorization(jwtAdapter)
+        const { jwtAdapter, sut } = makeSut()
         const decrypt = jest.spyOn(jwtAdapter, 'decrypt')
         await sut.verify('valid_token')
         expect(decrypt).toHaveBeenCalledWith('valid_token')
     })
     test('Should return value if token is valid', async () => {
-        const jwtAdapter = new JwtAdapter('secret')
-        const sut = new Authorization(jwtAdapter)
+        const { jwtAdapter, sut } = makeSut()
         jest.spyOn(jwtAdapter, 'decrypt').mockReturnValueOnce(Promise.resolve('token_valided'))
         const verify = await sut.verify('valid_token')
         expect(verify).toBe('token_valided')
     })
     test('Should throws if decrypt throws', async () => {
-        const jwtAdapter = new JwtAdapter('secret')
-        const sut = new Authorization(jwtAdapter)
+        const { jwtAdapter, sut } = makeSut()
         jest.spyOn(jwtAdapter, 'decrypt').mockImplementationOnce(throwError)
         const verify = sut.verify('valid_token')
         await expect(verify).rejects.toThrow()
