b74aaa7d8a13f066049d5013f857a05b1e4a36f7
Dinaerte Neto <dinaerteneto@gmail.com>
2021-11-05 14:55:20 -0300

    refactor: change import names of the interfaces

--- src/data/usecases/authentication.ts
+++ src/data/usecases/authentication.ts
@@ -1,15 +1,15 @@
-import { Authentication } from "@/domain/usecases/authentication"
+import { IAuthentication } from "@/domain/usecases"
 import { LoadAccountByEmailRepository } from "@/data/protocols/db/account"
 import { HashComparer, Encrypter } from "@/data/protocols/cryptography"
 
-export class Authencation implements Authentication {
+export class Authencation implements IAuthentication {
     constructor(
         private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
         private readonly hashComparer: HashComparer,
         private readonly encrypter: Encrypter
     ) { }
 
-    async auth(authentication: Authentication.Params): Promise<Authentication.Results> {
+    async auth(authentication: IAuthentication.Params): Promise<IAuthentication.Results> {
         const account = await this.loadAccountByEmailRepository.loadByEmail(authentication.email)
         if (account) {
             const isValid = await this.hashComparer.compare(authentication.password, account.password)
